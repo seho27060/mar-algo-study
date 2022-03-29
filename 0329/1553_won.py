694d1898b5fbabc5b7320d7351ce47b0c3470c42
jeongwon <yunjwon94@gmail.com>
2022-03-30 08:46:03 +0900

    0330 won

--- 0329/1553_won.py
+++ 0329/1553_won.py
@@ -1,15 +1,3 @@
-#
-#
-#
-# 수
-#
-# 리
-#
-# 중
-#
-#
-#
-
 arr = [list(map(int, input())) for _ in range(8)]
 # print(arr)
 
@@ -22,25 +10,30 @@ for i in range(7):
         domi.append([i, j])
 # print(domi)
 
-def f(i):
+def f():
     global cnt
-    if i == 28:
-        cnt += 1
-        return
     for si in range(8):
         for sj in range(7):
             if v[si][sj] == False:
-                for di, dj in [[0, 1], [1, 0]]:
-                    ni, nj = si + di, sj + dj
-                    if 0 <= ni < 8 and 0 <= nj < 7 and v[ni][nj] == False and ([arr[si][sj], arr[ni][nj]] in domi or [arr[ni][nj], arr[si][sj]] in domi):
-                        p = 0
-                        for i in range(len(domi)):
-                            if domi[i] == [arr[si][sj], arr[ni][nj]] or domi[i] == [arr[ni][nj], arr[si][sj]]:
-                                p = i
-                        tmp = domi.pop(p)
-                        v[si][sj] = v[ni][nj] = True
-                        f(i + 1)
-                        domi.append(tmp)
-                        v[si][sj] = v[ni][nj] = False
-f(0)
+                r(si, sj)
+                return
+        if not domi:
+            cnt += 1
+            return
+
+def r(si, sj):
+    for di, dj in [[0, 1], [1, 0]]:
+        ni, nj = si + di, sj + dj
+        if 0 <= ni < 8 and 0 <= nj < 7 and v[ni][nj] == False and ([arr[si][sj], arr[ni][nj]] in domi or [arr[ni][nj], arr[si][sj]] in domi):
+            p = 0
+            for k in range(len(domi)):
+                if domi[k] == [arr[si][sj], arr[ni][nj]] or domi[k] == [arr[ni][nj], arr[si][sj]]:
+                    p = k
+                    break
+            tmp = domi.pop(p)
+            v[si][sj] = v[ni][nj] = True
+            f()
+            domi.append(tmp)
+            v[si][sj] = v[ni][nj] = False
+r(0, 0)
 print(cnt)
